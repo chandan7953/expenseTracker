e357fbdd38574f45ce9d10388cb49f9ac9cb40d8
Chandan Singh <74015019+chandan7953@users.noreply.github.com>
2025-12-03 03:10:27 +0530

    route bug

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { useContext } from "react";
+import { AuthContext } from "./contexts/AuthProvider";
 import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
 import Leaderboard from "./pages/Leaderboard";
@@ -7,20 +9,88 @@ import Login from "./pages/Login";
 import Register from "./pages/Register";
 import ForgotPassword from "./pages/ForgotPassword";
 import ResetPassword from "./pages/ResetPassword";
+import { Navigate } from "react-router-dom";
 
 export default function App() {
+  const { loading, userId } = useContext(AuthContext);
+
+  const PublicRoute = ({ children }) => {
+    return !userId ? children : <Navigate to="/home" />;
+  };
+
+  const PrivateRoute = ({ children }) => {
+    return userId ? children : <Navigate to="/" />;
+  };
+
+  if (loading) {
+    return (
+      <div className="flex justify-center items-center h-screen">
+        <div className="animate-spin rounded-full h-12 w-12 border-b-2 border-blue-500"></div>
+        <span className="ml-3 text-gray-600">Loading...</span>
+      </div>
+    );
+  }
+
   return (
     <Router>
       <Navbar />
       <Routes>
-        <Route path="/" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/forgotpassword" element={<ForgotPassword />} />
-        <Route path="/resetpassword" element={<ResetPassword />} />
-
-        <Route path="/home" element={<Home />} />
-        <Route path="/leaderboard" element={<Leaderboard />} />
-        <Route path="/report" element={<ExpenseReport />} />
+        <Route
+          path="/"
+          element={
+            <PublicRoute>
+              <Login />
+            </PublicRoute>
+          }
+        />
+        <Route
+          path="/register"
+          element={
+            <PublicRoute>
+              <Register />
+            </PublicRoute>
+          }
+        />
+        <Route
+          path="/forgotpassword"
+          element={
+            <PublicRoute>
+              <ForgotPassword />
+            </PublicRoute>
+          }
+        />
+        <Route
+          path="/resetpassword"
+          element={
+            <PublicRoute>
+              <ResetPassword />
+            </PublicRoute>
+          }
+        />
+        <Route
+          path="/home"
+          element={
+            <PrivateRoute>
+              <Home />
+            </PrivateRoute>
+          }
+        />
+        <Route
+          path="/leaderboard"
+          element={
+            <PrivateRoute>
+              <Leaderboard />
+            </PrivateRoute>
+          }
+        />
+        <Route
+          path="/report"
+          element={
+            <PrivateRoute>
+              <ExpenseReport />
+            </PrivateRoute>
+          }
+        />
       </Routes>
     </Router>
   );
